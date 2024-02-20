21a3642f42d23f0f46fd78b4987ca12dac1be561
ian <ianmwendamicheni@gmail.com>
2024-02-20 12:40:24 +0300

    contact

---
+++ code/PHASE-4/Pizzas/frontend/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import Restaurant from './pages/Restaurant';
+import Pizzas from './pages/Pizzas';
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="restaurants/:id" element={<Restaurant />}/>
+            <Route path="pizzas" element={<Pizzas />} />
+            
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+     
+    </div>
+  );
+}
+
+export default App;
